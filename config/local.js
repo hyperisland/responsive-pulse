8a45a4376f3f5c048cb56c74724ff08364b014ae
Till Leinen <hello@till.is>
2016-12-16 14:13:35 -0800

    Add initial config file

---
+++ config/local.js
@@ -0,0 +1,20 @@
+module.exports = {
+  auth: {
+    secret: 'a secret',
+    adminPassword: 'an admin password'
+  },
+  session: {
+    secret: 'another secret'
+  },
+  email: {
+    from: 'an email address',
+    accessKeyId: 'an access key id',
+    secretAccessKey: 'a secret access key',
+    rateLimit: a_number,
+    region: 'a region'
+  },
+  url: {
+    production: 'a url',
+    staging: 'https://hyper-island-responsive-pulse.herokuapp.com/'
+  }
+};
